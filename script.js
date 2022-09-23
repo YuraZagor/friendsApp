92d98f8e23d6feae9402c1244e8bdd6162332e4e
YuraZagor <u.zagorulko@gmail.com>
2022-09-23 16:10:50 +0300

    it works. Css needed. Sorting needed

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 const url = 'https://randomuser.me/api/?results=10';
-let friendsDataArr = []
-// const outputElement = document.querySelector('.output')
+const friendCardsArr = []
+let friendsArr = []
+let sortedFriendCardsArr = [...friendCardsArr]
+
+const outputElement = document.querySelector('.output')
 
 
 const assignUsers = async () => {
 	try {
 		const response = await fetch(url);
 		const data = await response.json();
-		friendsDataArr = data.results;
-		console.log(friendsDataArr)
+		friendsArr = data.results;
+		cardsCreate(friendsArr)
+		console.log(friendsArr)
+		console.log(friendCardsArr)
 	} catch (error) {
-		renderErrorMessage('unable to connect, try again', error)
+		console.error(error)
 	} 
  }
  assignUsers()
 
 class FriendCard {
-	constructor(img, firstName, lastName, age, phone, country){
-		this.image = img;
+	constructor(image, firstName, lastName, age, phone, country){
+		this.image = image;
 		this.firstName = firstName;
 		this.lastName=lastName;
 		this.age=age;
@@ -27,21 +32,37 @@ class FriendCard {
 	render() {
 		outputElement.insertAdjacentHTML('beforeend', `
 			<li class="card">
-				<img class="usercard--img" src=${this.imgSrc} alt="">
-				<p class="usercard name">${this.firstName} ${this.lastName}</p>
-				<p class="usercard age">${this.age} years old</p>
-				<p class="usercard phone">${this.phone}</p>
+				<img class="friendcard--img" src=${this.image} alt="">
+				<p class="friendcard name">${this.firstName} ${this.lastName}</p>
+				<p class="friendcard age">${this.age} years old</p>
+				<p class="friendcard phone">${this.phone}</p>
+				<p class="friendcard country">${this.country}</p>
 			</li>`);
 		}
 } 
 
-function renderAllFriends(friendsDataArr){
-	friendsDataArr.map((friendObj)=> {
-		const {cell, gender} = friendObj
+function cardsCreate(friendsArr){
+	friendsArr.map((friendObj)=> {
+		const {cell} = friendObj
 		const { first, last } = friendObj.name
-		const imgSrc = friendObj.picture.medium;
+		const imgSrc = friendObj.picture.large;
 		const country = friendObj.location.country
 		const age = friendObj.dob.age;
-		new FriendCard(imgSrc, first, last, age, cell, country).render()
+		friendCardsArr.push(new FriendCard(imgSrc, first, last, age, cell, country))
 	})
+	friendCardsArr.forEach(card => card.render())
+}
+
+
+
+
+function sortingMin(param){
+	sortedFriendCardsArr.sort( (firstFriend, secondFriend) => {
+		return firstFriend.param - secondFriend.param
+	});
+}
+function sortingMax(param){
+	sortedFriendCardsArr.sort( (firstFriend, secondFriend) => {
+		return secondFriend.param - firstFriend.param 
+	});
 }
